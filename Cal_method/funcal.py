3bf9874815ac1c598edc08bbeebec85b59f4b5e8
mundo <https://github.com/ray-rain-mundo/test.git>
2020-10-15 21:14:56 +0800

    pytest 第一次作业

---
+++ Cal_method/funcal.py
@@ -0,0 +1,9 @@
+class Calculator:
+
+    def add(self, a, b):
+        self.result_add = a + b
+        return self.result_add
+
+    def div(self, a, b):
+        self.result_div = a / b
+        return self.result_div
